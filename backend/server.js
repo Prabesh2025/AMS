bf4e072defea5447b5633bc48d1d50c29b8caf2b
Prabesh2025 <prabeshneupane231@gmail.com>
2025-05-06 13:37:32 +0545

    update in cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // 2.Middleware
 app.use(express.json());
 app.use(cors({
-     origin:["http://localhost:5173", "https://ams-six-rho.vercel.app/"],
+  origin:["http://localhost:5173", "https://ams-six-rho.vercel.app"],
 }));
 
 app.get("/", (req, res) => {
